42d94ba4663bfd91a64bf890decb755ad77a9e1b
Thomas Li Fredriksen <thomas.fredriksen@oceandata.earth>
2022-05-19 11:02:51 +0200

    Converted to streamlit template

--- {{cookiecutter.project_slug}}/odp/app/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/odp/app/{{cookiecutter.project_slug}}/__main__.py
@@ -1,33 +1,22 @@
-from flask import Flask
-import click
+"""Top-level code-execution for streamlit applicatication
 
-from typing import *
+Allows a streamlit app to be run as a module:
+
+```sh
+python -m odp.app.{{cookiecutter.project_slug}}
+```
+"""
+import sys
+from streamlit import cli as stcli
+import streamlit as st
+from odp.app.{{cookiecutter.project_slug}}.app import main as streamlit_entry
 
 
-app = Flask("Hello world")
-
-@app.route("/")
-def root_endpoint() -> str:
-    return "<p>Hello, World</p>"
-
-@click.command()
-@click.option("--host", default="0.0.0.0", help="The hostname to listen on")
-@click.option("--port", default=None, help="The port of the webserver")
-@click.option("--debug", is_flag=True, help="Enable debug-mode")
-@click.option("--load-dotenv/--no-load-dotenv", default=True,
-              help="Load the nearest .env and .flaskenv files to set environment variables")
-def main(
-        host: Optional[str],
-        port: Optional[int],
-        debug: Optional[bool],
-        load_dotenv: bool,
-) -> None:
-    app.run(
-        host=host,
-        port=port,
-        debug=debug,
-        load_dotenv=load_dotenv
-    )
 
 if __name__ == "__main__":
-    main()
+    if st._is_running_with_streamlit:
+        streamlit_entry()
+    else:
+        sys.argv = ["streamlit", "run", sys.argv[0]]
+        sys.exit(stcli.main())
+
---
+++ {{cookiecutter.project_slug}}/odp/app/{{cookiecutter.project_slug}}/app.py
@@ -0,0 +1,11 @@
+"""Streamlit App
+
+Put your streamlit code here
+"""
+import streamlit as st
+
+
+def main():
+    st.title("Hello world")
+    st.write("This is a streamlit app")
+
